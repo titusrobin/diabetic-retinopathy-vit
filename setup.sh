ff74169273ca3a479277becbf9cb266f689668b6
Robin Titus <143838819+titusrobin@users.noreply.github.com>
2023-12-07 20:50:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env bash
-virtualenv ~/.venv
-source ~/.venv/bin/activate
-make install
+# #!/usr/bin/env bash
+# virtualenv ~/.venv
+# source ~/.venv/bin/activate
+# make install
 
-#append it to bash so every shell launches with it 
-echo 'source ~/.venv/bin/activate' >> ~/.bashrc
+# #append it to bash so every shell launches with it 
+# echo 'source ~/.venv/bin/activate' >> ~/.bashrc
+
+pip install -r requirements.txt
+
+uvicorn app:app --host 0.0.0.0 --port 8000
