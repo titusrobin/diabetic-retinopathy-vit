4337f41c086c12713048decd0eb320cb047c8498
AliciaXia222 <143651934+AliciaXia222@users.noreply.github.com>
2023-12-09 12:53:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from PIL import Image
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 import torch
 import pandas as pd
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Load model and processor
-processor = AutoImageProcessor.from_pretrained(
-    "rafalosa/diabetic-retinopathy-224-procnorm-vit"
-)
-model = AutoModelForImageClassification.from_pretrained(
-    "rafalosa/diabetic-retinopathy-224-procnorm-vit"
-)
+processor = AutoImageProcessor.from_pretrained("rafalosa/diabetic-retinopathy-224-procnorm-vit")
+model = AutoModelForImageClassification.from_pretrained("rafalosa/diabetic-retinopathy-224-procnorm-vit")
 
 # Streamlit app
 st.title("Image Classification App")
@@ -20,6 +20,8 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 # Display the uploaded image
 if uploaded_file is not None:
+    logging.info("Image uploaded successfully.")
+
     # Open the image file
     image = Image.open(uploaded_file)
 
@@ -28,25 +30,32 @@ if uploaded_file is not None:
 
     # Predict button
     if st.button("Predict"):
-        # Use the image processor to convert the image to a PyTorch tensor
-        inputs = processor(images=image, return_tensors="pt")
+        logging.info("Starting prediction.")
+        
+        try:
+            # Use the image processor to convert the image to a PyTorch tensor
+            inputs = processor(images=image, return_tensors="pt")
 
-        # Perform inference
-        outputs = model(**inputs)
+            # Perform inference
+            outputs = model(**inputs)
 
-        # Get the predicted class probabilities
-        logits = outputs.logits
-        probabilities = torch.nn.functional.softmax(logits, dim=-1)
+            # Get the predicted class probabilities
+            logits = outputs.logits
+            probabilities = torch.nn.functional.softmax(logits, dim=-1)
 
-        # Your other code remains the same...
+            # Your other code remains the same...
 
-        # Assuming probabilities_list contains the probabilities for each category
-        categories = ["Mild", "Moderate", "Healthy", "Severe", "Proliferative"]
-        probabilities_list = probabilities.tolist()[0]  # Example list of probabilities
-        data = pd.DataFrame(
-            probabilities_list, index=categories, columns=["probability"]
-        )
+            # Assuming probabilities_list contains the probabilities for each category
+            categories = ["Mild", "Moderate", "Healthy", "Severe", "Proliferative"]
+            probabilities_list = probabilities.tolist()[0]  # Example list of probabilities
+            data = pd.DataFrame(
+                probabilities_list, index=categories, columns=["probability"]
+            )
 
-        # Use Streamlit to create a bar chart
-        st.bar_chart(data)
+            # Use Streamlit to create a bar chart
+            st.bar_chart(data)
 
+            logging.info("Prediction completed successfully.")
+        except Exception as e:
+            logging.error(f"Error during prediction: {e}")
+            st.error("An error occurred during prediction.")
