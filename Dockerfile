f4230f4eed85eafaf972c662c96a2db565fcc92f
rtitusa <robintitus10@gmail.com>
2023-12-10 17:54:15 -0500

    Added streamlit next level integration

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,5 @@
-# # Use the official Python base image
-# FROM python:3.8
+FROM ubuntu:latest
+RUN apk update && apk add bash
 
-# # Set the working directory in the container
-# WORKDIR /app
-
-# # Copy the dependencies file to the working directory
-# COPY requirements.txt .
-
-# # Install any dependencies
-# RUN pip install --no-cache-dir -r requirements.txt
-
-# # Copy the content of the local src directory to the working directory
-# COPY . .
-
-# # Specify the command to run on container start
-# CMD streamlit run app.py --server.port $PORT
-# Use an official Python runtime as a parent image
-FROM python:3.10.8
-
-# Set the working directory to /app
 WORKDIR /app
-
-# Copy the current directory contents into the container at /app
-COPY . /app
-
-# Install any needed packages specified in requirements.txt
-RUN pip install -r requirements.txt
-
-# Make port 5000 available to the world outside this container
-EXPOSE 5000
-
-# Define environment variable
-ENV NAME World
-
-# Run app.py when the container launches
-CMD ["python", "app.py", "--host=0.0.0.0", "--port=5000"]
+COPY repeat.sh /app
--- app.py
+++ app.py
@@ -1,94 +1,61 @@
-from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-from PIL import Image, UnidentifiedImageError
+import streamlit as st
+from PIL import Image
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 import torch
 import pandas as pd
 import logging
-import os
-
-# Initialize Flask app
-app = Flask(__name__)
-
-UPLOAD_FOLDER = "static/uploads"
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # Configure logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Load model and processor
-processor = AutoImageProcessor.from_pretrained(
-    "rafalosa/diabetic-retinopathy-224-procnorm-vit"
-)
-model = AutoModelForImageClassification.from_pretrained(
-    "rafalosa/diabetic-retinopathy-224-procnorm-vit"
-)
+processor = AutoImageProcessor.from_pretrained("rafalosa/diabetic-retinopathy-224-procnorm-vit")
+model = AutoModelForImageClassification.from_pretrained("rafalosa/diabetic-retinopathy-224-procnorm-vit")
 
+# Streamlit app
+st.title("Image Classification App")
 
-@app.route("/", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        if "file" not in request.files:
-            logging.error("No file part")
-            return redirect(request.url)
-        file = request.files["file"]
-        if file.filename == "":
-            logging.error("No selected file")
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            file.save(filepath)
-            return redirect(url_for("predict", filename=filename))
-    return render_template("upload.html")
+# Upload image through Streamlit
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
+# Display the uploaded image
+if uploaded_file is not None:
+    logging.info("Image uploaded successfully.")
 
-@app.route("/predict/<filename>")
-def predict(filename):
-    filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-    try:
-        # Open the image file
-        image = Image.open(filepath)
+    # Open the image file
+    image = Image.open(uploaded_file)
 
-        # Use the image processor to convert the image to a PyTorch tensor
-        inputs = processor(images=image, return_tensors="pt")
+    # Display the uploaded image
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-        # Perform inference
-        outputs = model(**inputs)
+    # Predict button
+    if st.button("Predict"):
+        logging.info("Starting prediction.")
+        
+        try:
+            # Use the image processor to convert the image to a PyTorch tensor
+            inputs = processor(images=image, return_tensors="pt")
 
-        # Get the predicted class probabilities
-        logits = outputs.logits
-        probabilities = torch.nn.functional.softmax(logits, dim=-1)
-        categories = ["Mild", "Moderate", "Healthy", "Severe", "Proliferative"]
-        probabilities_list = probabilities.tolist()[0]  # Example list of probabilities
+            # Perform inference
+            outputs = model(**inputs)
 
-        # Logging
-        logging.info("Prediction completed successfully.")
+            # Get the predicted class probabilities
+            logits = outputs.logits
+            probabilities = torch.nn.functional.softmax(logits, dim=-1)
 
-        # Check if the retina is healthy
-        health_probability = probabilities_list[categories.index("Healthy")]
-        is_healthy = health_probability > 0.5
-        health_message = "This retina is healthy !!!" if is_healthy else ""
+            # Your other code remains the same...
 
-        return render_template(
-            "prediction.html",
-            categories=categories,
-            probabilities=probabilities_list,
-            image_url=url_for("static", filename="uploads/" + filename),
-            health_message=health_message,
-        )
+            # Assuming probabilities_list contains the probabilities for each category
+            categories = ["Mild", "Moderate", "Healthy", "Severe", "Proliferative"]
+            probabilities_list = probabilities.tolist()[0]  # Example list of probabilities
+            data = pd.DataFrame(
+                probabilities_list, index=categories, columns=["probability"]
+            )
 
-    except UnidentifiedImageError:
-        return render_template(
-            "error.html", error="This is not a standard retinopathy picture"
-        )
-    except Exception as e:
-        logging.error(f"Error during prediction: {e}")
-        return render_template("error.html", error=str(e))
+            # Use Streamlit to create a bar chart
+            st.bar_chart(data)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+            logging.info("Prediction completed successfully.")
+        except Exception as e:
+            logging.error(f"Error during prediction: {e}")
+            st.error("An error occurred during prediction.")
